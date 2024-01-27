0442307f54124b4cde0daab452258614bcb0c153
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-01-27 14:41:51 +0100

    modify folder

--- enitities/mob/Zombie.gd
+++ enitities/mob/Zombie.gd
@@ -3,19 +3,17 @@ class_name Zombie
 
 @onready var player = get_parent().get_node("%Player")
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	(%Vision as Area2D).body_entered.connect(seeSomething)
 	(%AttackArea).body_entered.connect(attack)
-	pass # Replace with function body.
 
 
 func toggleRun(value: bool):
 	super.toggleRun(value)
 	set_collision_layer_value(5, value)
-	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	super._process(delta)
 	if target == null:
@@ -26,6 +24,7 @@ func seeSomething(body):
 		target = body
 	elif body.position.distance_to(position) < target.position.distance_to(position):
 		target = body
+
 
 func attack(body):
 	print("attack", body)
