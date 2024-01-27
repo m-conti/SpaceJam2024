85a6e08fb576082da617a7fe9a8968646eba0590
mconti <mc.heux@gmail.com>
2024-01-27 13:19:47 +0100

    attack

--- enitities/character/Character.gd
+++ enitities/character/Character.gd
@@ -2,15 +2,22 @@ extends CharacterBody2D
 
 
 @export var walkSpeed: float = 6000.0
-@export var runSpeed: float = 10000.0
+@export var runSpeed: float = 16000.0
 @export var stamina: float = 10.0
 var isRunning: bool = false
+@export var attack_speed: float = 0.5:
+	set(value): attackTimer.wait_time = 1 / value
+
+@onready var attackTimer: Timer = Timer.new()
 
 var speed: float:
 	get: return runSpeed if isRunning else walkSpeed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	self.add_child(attackTimer)
+	attackTimer.wait_time = 1 / attack_speed
+	attackTimer.one_shot = true
 	pass # Replace with function body.
 
 func _input(event):
@@ -18,7 +25,8 @@ func _input(event):
 		self.toggleRun(true)
 	if event.is_action_released("run"):
 		self.toggleRun(false)
-
+	if event.is_action_pressed("attack"):
+		askToAttack()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -41,3 +49,17 @@ func _physics_process(delta):
 		direction.y -= 1
 	velocity = direction.normalized() * speed * delta
 	move_and_slide()
+
+func attack():
+	var bodies = (%AttackArea as Area2D).get_overlapping_bodies()
+	if bodies.size() == 0: return
+	var to_kill = bodies[0]
+	to_kill.attacked.emit()
+
+func askToAttack():
+	if attackTimer.is_stopped():
+		attackTimer.timeout.disconnect(askToAttack)
+		attackTimer.start(attack_speed)
+		attack()
+	elif not attackTimer.timeout.is_connected(askToAttack):
+		attackTimer.timeout.connect(askToAttack)
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -9,31 +9,31 @@ extends Timer
 
 @export var spawn_range := Vector2i(5, 5)
 
-@onready var map: Map = get_parent()
+@onready var map: Map = %Map
 
 
 func _on_timeout() -> void:
-    spawn_around(%Player)
+	spawn_around(%Player)
 
 
 func get_spawnable_map_pos_around(map_pos: Vector2i, min_radius: Vector2i, max_radius: Vector2i) -> Vector2i:
-    var spawnable = false
-    var spawnable_map_pos := Vector2i()
+	var spawnable = false
+	var spawnable_map_pos := Vector2i()
 
-    while not spawnable:
-        spawnable_map_pos = map_pos + Vector2i(randi_range(min_radius.x, max_radius.x), randi_range(min_radius.y, max_radius.y))
-        spawnable = map.can_move(spawnable_map_pos)
-    
-    return spawnable_map_pos
+	while not spawnable:
+		spawnable_map_pos = map_pos + Vector2i(randi_range(min_radius.x, max_radius.x), randi_range(min_radius.y, max_radius.y))
+		spawnable = map.can_move(spawnable_map_pos)
+	
+	return spawnable_map_pos
 
 
 func spawn_around(entity: Node2D) -> void:
-    var spawnable_map_pos: Vector2i = get_spawnable_map_pos_around(map.local_to_map(entity.position), min_spawn_dist, max_spawn_dist)
+	var spawnable_map_pos: Vector2i = get_spawnable_map_pos_around(map.local_to_map(entity.position), min_spawn_dist, max_spawn_dist)
 
-    var nb_zombies = randi_range(spawn_count.x, spawn_count.y)
-    for i in range(nb_zombies):
-        spawnable_map_pos = get_spawnable_map_pos_around(spawnable_map_pos, -spawn_range, spawn_range)
+	var nb_zombies = randi_range(spawn_count.x, spawn_count.y)
+	for i in range(nb_zombies):
+		spawnable_map_pos = get_spawnable_map_pos_around(spawnable_map_pos, -spawn_range, spawn_range)
 
-        var entity_instance = entity_to_spawn.instantiate()
-        entity_instance.position = map.map_to_local(spawnable_map_pos)
-        map.add_child(entity_instance)
+		var entity_instance = entity_to_spawn.instantiate()
+		entity_instance.global_position = map.to_global(map.map_to_local(spawnable_map_pos))
+		map.get_parent().add_child(entity_instance)
