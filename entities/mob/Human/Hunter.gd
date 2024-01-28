1cc847d38ae6beb5f0c4acb083ba9242756ec5e3
mconti <mc.heux@gmail.com>
2024-01-28 14:39:25 +0100

    power + life

--- entities/mob/Human/Hunter.gd
+++ entities/mob/Human/Hunter.gd
@@ -6,16 +6,16 @@ extends Human
 
 
 static func get_score():
-    return 4
+	return 4
 
 
 func change_target_mode():
-    if target == null:
-        return
+	if target == null:
+		return
 
-    var distance = target_pos.distance_to(position)
-    
-    if distance > attack_range:
-        targetMode = ETargetMode.ATTACK
-    else:
-        targetMode = ETargetMode.FLEE
+	var distance = target_pos.distance_to(position)
+	
+	if distance > attack_range:
+		targetMode = ETargetMode.ATTACK
+	else:
+		targetMode = ETargetMode.FLEE
