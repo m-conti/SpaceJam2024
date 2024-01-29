da7c3e630380786189b7ea327272e45e26dfa175
mconti <mc.heux@gmail.com>
2024-01-29 19:22:41 +0100

    fix name

--- UI/playerName/player_name.gd
+++ UI/playerName/player_name.gd
@@ -4,8 +4,13 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	get_parent().name_already_used.connect(name_already_used)
 	pass # Replace with function body.
 
+
+func name_already_used():
+	(%AlreadyUsed as Label).modulate = Color(255, 255, 255, 255)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,4 +21,9 @@ func _on_button_pressed():
 	var new_name = %Name.text
 	if new_name != "":
 		get_parent().name_choosed.emit(new_name)
+	pass # Replace with function body.
+
+
+func _on_name_text_changed(new_text):
+	(%AlreadyUsed as Label).modulate = Color(255, 255, 255, 0)
 	pass # Replace with function body.
